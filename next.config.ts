cad03b4c38d0c8362ae99ff99323f0ee47bad8c4
Kyler Chavez <kchavez05@gmail.com>
2025-07-24 15:23:52 +0000

    Still displays as broken. I will publish our changes to see if it's just

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
